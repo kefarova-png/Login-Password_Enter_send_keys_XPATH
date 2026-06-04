d7a47c973b434fb875225d76f7af047e6a437153
kefarova-png <kefarova@gmail.com>
2026-06-04 12:27:35 +0400

    Вместо ENTER жмём кнопку авторизации

--- Login-Password_Enter_send_keys_XPATH.py
+++ Login-Password_Enter_send_keys_XPATH.py
@@ -21,44 +21,43 @@ driver.get('https://saucedemo.com/')
 #  Устанавливаем размер окна
 driver.set_window_size(1920,1080)
 
-#  Находим элемент для логина, используя XPATH, и Вводим в поле логин "standard_user"
-driver.find_element(By.XPATH, "//input[@id='user-name']").send_keys("standard_user")
+#  Находим элемент для логина, используя XPATH, и Вводим в поле логина "to del using clear"
+user_name = driver.find_element(By.XPATH, "//input[@id='user-name']")
+user_name.send_keys("to del using clear")
+print('Login input')
+
+#  Находим элемент для пароля, используя XPATH, и Вводим в поле пароль "to delete using Ctrl+'a' & DELETE"
+user_password = driver.find_element(By.XPATH, "//input[@id='password']")
+user_password.send_keys("to delete using Ctrl+'a' & DELETE")
+print('Password input')
+time.sleep(3)  #  Для визуального контроля за происходящим
+
+#  Очистим содержимое поля логина
+user_name.clear()
+print('Login cleared')
+time.sleep(2)  #  Для визуального контроля за происходящим
+
+#  Вводим в поле логин "standard_user"
+user_name.send_keys("standard_user")
 print('Login input')
 time.sleep(2)  #  Для визуального контроля за происходящим
 
-#  Выделим содержиое поля логина и удалим его
-driver.find_element(By.XPATH, "//input[@id='user-name']").send_keys(Keys.CONTROL + 'a')
-print('Login selected')
-time.sleep(2)  #  Для визуального контроля за происходящим
-driver.find_element(By.XPATH, "//input[@id='user-name']").send_keys(Keys.BACKSPACE)
-print('Login removed')
-
-#  Находим элемент для пароля, используя XPATH, и Вводим в поле пароль "secret_sauce_"
-driver.find_element(By.XPATH, "//input[@id='password']").send_keys("secret_sauce_")
-print('Password input')
-time.sleep(2)  #  Для визуального контроля за происходящим
-
-#  Выделим содержиое поля пароля и удалим его
-driver.find_element(By.XPATH, "//input[@id='password']").send_keys(Keys.CONTROL + 'a')
+#  Выделим содержимое поля пароля и удалим его
+user_password.send_keys(Keys.CONTROL + 'a')
 print('Password selected')
 time.sleep(2)  #  Для визуального контроля за происходящим
-driver.find_element(By.XPATH, "//input[@id='password']").send_keys(Keys.DELETE)
+user_password.send_keys(Keys.DELETE)
 print('Password removed')
 time.sleep(2)  #  Для визуального контроля за происходящим
 
-#  Нажмём кнопку ENTER на клавиатуре при активном поле ввода пароля
-driver.find_element(By.XPATH, "//input[@id='password']").send_keys(Keys.ENTER)
-print('ENTER pressed')
+#  Вводим в поле пароля "secret_sauce"
+user_password.send_keys("secret_sauce")
+print('Password input')
 time.sleep(2)  #  Для визуального контроля за происходящим
 
-# #  Найдём и нажмём кнопку входа
-# driver.find_element(By.XPATH, "//input[@id='login-button']").click()
-# print('Login button click')
-
-#  Обновляем страницу
-driver.refresh()
-print("Page refresh")
-time.sleep(2)  #  Для визуального контроля за происходящим
+#  Жмём кнопку авторизации
+driver.find_element(By.XPATH, "//input[@id='login-button']").click()
+time.sleep(3)  #  Для визуального контроля за происходящим
 
 #  Закрываем браузер
 driver.close()
